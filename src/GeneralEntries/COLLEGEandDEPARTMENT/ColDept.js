5ee0eeeb7ffd1aaa3b0aeb3a9ff3b3ba0f663ca3
Loida Mae <idaungab@icloud.com>
2017-12-04 15:17:01 +0800

    Insert college data using "fetch"

--- src/GeneralEntries/COLLEGEandDEPARTMENT/ColDept.js
+++ src/GeneralEntries/COLLEGEandDEPARTMENT/ColDept.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Table, Column, Cell} from 'fixed-data-table-2';
+import axios from 'axios';
 
 import '../.././style/App.css';
 import 'fixed-data-table-2/dist/fixed-data-table.css';
 
+import Button from '../.././Layout/Button';
 import DropdownList from '../.././Layout/DropList';
 import Input from '../.././Layout/BasicInput';
+import Select from '../.././Layout/Select';
 
 class ColDept extends React.Component{
 
@@ -14,14 +17,18 @@ class ColDept extends React.Component{
 
     this.state ={
       college : [],
-      coldept: []
+      coldept: [],
+      isActive:true,
+      updateColcode:"",
+      updateCollege:"",
+      url: "http://192.168.5.146:3000/"
     }
   }
   componentDidMount(){
 
     console.log('COMPONENT HAS MOUNTED');
     //fetch PROGRAM data
-    fetch('http://192.168.5.146:3000/COLLEGEandDEPARTMENT')
+    fetch(this.state.url + 'COLLEGEandDEPARTMENT')
       .then(response => response.json())
           .then(data => {
             this.setState({
@@ -31,7 +38,7 @@ class ColDept extends React.Component{
            })
            .catch( error => console.log('Error Fetch: college ' + error))
 
-     fetch('http://192.168.5.146:3000/college')
+     fetch(this.state.url + 'college')
        .then(response => response.json())
            .then(data => {
              this.setState({
@@ -40,6 +47,52 @@ class ColDept extends React.Component{
              console.log(data);
             })
             .catch( error => console.log('Error Fetch: college ' + error))
+
+  }
+
+  addCollege(e){
+      e.preventDefault();
+          let college = this.state.college;
+          let college_data={
+            colcode: this.state.updateColcode,
+            college: this.state.updateCollege,
+            empid: "M01 - 002"
+          };
+        
+          var request = new Request(this.state.url + 'addCollege',{
+            method:'POST',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify(college_data)
+          });
+
+          fetch(request)
+          .then((response) => {
+            console.log(college_data);
+            college.push(college_data);
+            console.log(college);
+            this.setState({
+              college: college
+            })
+            response.json()
+            .then((data)=>{
+            })
+          })
+          .catch(function(err){
+            console.log(err);
+          })
+  }
+
+
+  handleSelectChange(event){
+    this.setState({isActive: event.target.checked});
+    console.log(event.target.checked);
+  }
+
+  handleColcodeChange(e){
+    this.setState({updateColcode: e.target.value});
+  }
+  handleCollegeChange(e){
+    this.setState({updateCollege: e.target.value});
   }
 
 
@@ -51,17 +104,20 @@ class ColDept extends React.Component{
       <div className="row">
         <div className="UpperEntry">
           <h4>COLLEGE ENTRY</h4>
-          <Input name="colcode" label="College code" type="text" placeholder="Enter college code"  />
-          <Input name="college" label="College" type="text" placeholder="Enter college name"  />
+          <Input name="colcode" label="College code" type="text" placeholder="Enter college code"  value={this.state.updateColcode} onChange={this.handleColcodeChange.bind(this)} />
+          <Input name="college" label="College" type="text" placeholder="Enter college name"  value={this.state.updateCollege} onChange={this.handleCollegeChange.bind(this)} />
+          <Button btnName="Add" onClick={this.addCollege.bind(this)}/>
         </div>
         <div className="UpperEntry">
           <h4>DEPARTMENT ENTRY</h4>
           <DropdownList data={col} name="college" label="College" />
-          <Input name="deptcode" label="Department code" type="text" placeholder="Enter department code"  />
-          <Input name="deptname" label="Department name" type="text" placeholder="Enter department name"  />
+        </div>
+        <div className="UpperEntry">
+          <Input name="deptcode" label="Department code" type="text" placeholder="Enter department code" ref="deptcode" />
+          <Input name="deptname" label="Department name" type="text" placeholder="Enter department name" ref="deptname" />
+          <Select label="Active" checked={this.state.isActive} onChange={this.handleSelectChange.bind(this)} />
         </div>
         <div className="DataView">
-          {/* <TabularData  rowsCount={program.length} data={program} twidth={1050} header1="CODE" header2="PROGRAM" header3="MAJOR" header4="DEPARTMENT"/> */}
           <Table
               rowsCount={college.length}
               rowHeight={50}
