b5713b9327e4bd61b69e00bcaa3a4addbbb13264
Loida Mae <idaungab@icloud.com>
2017-12-12 16:34:16 +0800

    Displaying "Program" using react's fixed-data-table 2 Add new data functionality

--- src/GeneralEntries/Program/Program.js
+++ src/GeneralEntries/Program/Program.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Table, Column, Cell} from 'fixed-data-table-2';
+import axios from 'axios';
 
 import Button from '../.././Layout/Button';
 import DropdownList from '../.././Layout/DropList';
@@ -12,23 +13,26 @@ import '../.././style/App.css';
 import '../.././style/bootstrap.css';
 import 'fixed-data-table-2/dist/fixed-data-table.css';
 
+
 class Program extends React.Component{
 
   constructor(props){
-    super();
+    super(props);
     this.state = {
       program: [],
       department:[],
       deptData:[],
       searchTerm:"",
-      level:['Undergraduate','Masteral','Doctorate'],
-      radioValue:"Masteral",
-      isActive: false
+      isUndergrad:false,
+      isMasteral:false,
+      isDoctorate:false,
+      isActive: true,
+      programCode:"",
+      programdesc:"",
+      major:"",
+      deptListData:"--Select",
+      url:"http://192.168.5.146:3000/"
     }
-
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.handleRadioChange = this.handleRadioChange.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
 }
 
 //fetch program data
@@ -57,8 +61,45 @@ class Program extends React.Component{
             console.log(this.state.department);
            })
            .catch( error => console.log('Error Fetch: department ' + error))
+  }
 
-    console.log(this.state.isActive);
+  addProgram(e){
+    e.preventDefault();
+
+    let program = {
+      progcode:this.state.programCode,
+      progdesc:this.state.programdesc,
+      is_active:this.state.isActive,
+      undergrad:this.state.isUndergrad,
+      progdept:this.state.deptListData,
+      major:this.state.major,
+      masteral:this.state.isMasteral,
+      phd:this.state.isDoctorate,
+      shorthand:"BSUNSA"
+    };
+    axios.post(this.state.url + 'addProgram',
+        program
+      )
+      .then(response => {
+        console.log(response);
+        alert('New Data Added');
+      })
+      .catch(error => {
+        console.log(error.response);
+        console.log(program);
+      });
+  }
+
+  handleProgcodechange(e){
+    this.setState({programCode: e.target.value});
+  }
+
+  handleProgramChange(e){
+    this.setState({programdesc:e.target.value.toUpperCase()});
+  }
+
+  handleMajorChange(e){
+    this.setState({major: e.target.value});
   }
 
   //get data searched
@@ -72,8 +113,20 @@ class Program extends React.Component{
     this.setState({radioValue: r.target.value});
   }
 
-  handleSelectChange(event){
+  handleIsactiveChange(event){
     this.setState({isActive: event.target.checked});
+  }
+
+  handleIsundergradChange(e){
+    this.setState({isUndergrad:e.target.checked});
+  }
+
+  handleIsmasteralChange(e){
+    this.setState({isMasteral:e.target.checked});
+  }
+
+  handleIsdoctorateChange(e){
+    this.setState({isDoctorate:e.target.checked});
   }
 
   render(){
@@ -83,22 +136,79 @@ class Program extends React.Component{
     return(
       <div className="row">
         <div className="Entry">
-          <Search name="search" placeholder="Search program. . ."  value={this.state.searchTerm} onChange={this.handleSearchChange}/>
-          <DropdownList data={depart} name="department" label="Department" />
-          <Input name="programCode" label="Program Code: " type="text" placeholder="Program Code" />
-          <Select label="Active" checked={this.state.isActive} onChange={this.handleSelectChange}/>
-          <Input name="program" label="Program" type="text" placeholder="Program" />
-          <Input name="major" label="Major" type="text" placeholder="Major"/>
-          <RadioButton options={this.state.level} name="level" value={this.state.radioValue} onChange={this.handleRadioChange}/>
-          <Button btnName="Add" />
-          <Button btnName="Save" />
-          <Button btnName="Cancel" />
+            <DropdownList
+              data={depart}
+              name="department"
+              label="Department"
+              value={this.state.deptListData}
+              onChange={value => this.setState({ deptListData: value })} />
 
+            <Input
+              name="programCode"
+              label="Program Code: "
+              type="text"
+              placeholder="Program Code"
+              value={this.state.programCode}
+              onChange={this.handleProgcodechange.bind(this)}/>
+
+            <Select
+                label="Active"
+                checked={this.state.isActive}
+                onChange={this.handleIsactiveChange.bind(this)}/>
+
+            <Input
+              name="program"
+              label="Program"
+              type="text"
+              placeholder="Program"
+              value={this.state.programdesc}
+              onChange={this.handleProgramChange.bind(this)} />
+
+            <Input
+              name="major"
+              label="Major"
+              type="text"
+              placeholder="Major"
+              value={this.state.major}
+              onChange={this.handleMajorChange.bind(this)}/>
+
+            <Select
+              label="Undergraduate"
+              checked={this.state.isUndergrad}
+              onChange={this.handleIsundergradChange.bind(this)}/>
+
+            <Select
+              label="Masteral"
+              checked={this.state.isMasteral}
+              onChange={this.handleIsmasteralChange.bind(this)}/>
+
+            <Select
+              label="Doctorate"
+              checked={this.state.isDoctorate}
+              onChange={this.handleIsdoctorateChange.bind(this)}/>
+
+            <Button
+              btnName="Add"
+              onClick={this.addProgram.bind(this)}/>
+
+            <Button
+              btnName="Edit" />
+
+            <Button
+              btnName="Save" />
+
+            <Button
+              btnName="Cancel" />
         </div>
         <div className="DataView">
+          <Search
+            name="search"
+            placeholder="Search program. . ."
+            value={this.state.searchTerm}
+            onChange={this.handleSearchChange.bind(this)} />
           {/* <TabularData  rowsCount={program.length} data={program} twidth={1050} header1="CODE" header2="PROGRAM" header3="MAJOR" header4="DEPARTMENT"/> */}
           <Table
-              rowsCount={program.length}
+              rowsCount={this.state.program.length}
               rowHeight={50}
               width={1050}
               height={600}
