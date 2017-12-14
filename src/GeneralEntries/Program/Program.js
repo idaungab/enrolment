aa8960fe1094c346db5f584412ffb52992f9256c
Loida Mae <idaungab@icloud.com>
2017-12-14 17:50:28 +0800

    Iconized and added Modal (for modifications)

--- src/GeneralEntries/Program/Program.js
+++ src/GeneralEntries/Program/Program.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {Table, Column, Cell} from 'fixed-data-table-2';
+// import {Table, Column, Cell} from 'fixed-data-table-2';
+import {BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import axios from 'axios';
+import Modal from 'react-modal';
 
 import Button from '../.././Layout/Button';
 import DropdownList from '../.././Layout/DropList';
+import Header from '../.././Layout/Header';
 import Input from '../.././Layout/BasicInput';
-import RadioButton from '../.././Layout/RadioButtonGroup';
 import Search from '../.././Layout/Search';
 import Select from '../.././Layout/Select';
 
+import addIcon from '../../images/icons/add.svg';
+import cancelIcon from '../../images/icons/cancel.svg';
+import editIcon from '../../images/icons/edit.svg';
+import saveIcon from '../../images/icons/save.svg';
+
 import '../.././style/App.css';
-import '../.././style/bootstrap.css';
+import '../.././style/bootstrap.min.css';
 import 'fixed-data-table-2/dist/fixed-data-table.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 
 class Program extends React.Component{
@@ -31,6 +39,7 @@ class Program extends React.Component{
       programdesc:"",
       major:"",
       deptListData:"--Select",
+      modalIsOpen: false,
       url:"http://192.168.5.146:3000/"
     }
 }
@@ -88,6 +97,33 @@ class Program extends React.Component{
         console.log(error.response);
         console.log(program);
       });
+
+  }
+
+  saveEdit(e){
+    e.preventDefault();
+    let program = {
+      progcode:this.state.programCode,
+      progdesc:this.state.programdesc,
+      is_active:this.state.isActive,
+      undergrad:this.state.isUndergrad,
+      progdept:this.state.deptListData,
+      major:this.state.major,
+      masteral:this.state.isMasteral,
+      phd:this.state.isDoctorate,
+      shorthand:"BSUNSA"
+    };
+    axios.post(this.state.url + 'updateProgram',
+        program
+      )
+      .then(response => {
+        console.log(response);
+        alert('Data updated');
+      })
+      .catch(error => {
+        console.log(error.response);
+        console.log(program);
+      });
   }
 
   handleProgcodechange(e){
@@ -129,85 +165,160 @@ class Program extends React.Component{
     this.setState({isDoctorate:e.target.checked});
   }
 
+ handleRowSelect(row, isSelected, e){
+   let departCode = this.state.department.map(obj => obj.deptcode);
+   let departName = this.state.department.map(obj => obj.deptname);
+
+   for(var i=0; i < this.state.department.length; i++){
+      if(departCode[i] === row.progdept){
+        this.setState({deptListData: departName[i]});
+      }
+   }
+
+   this.setState({
+     programCode: row.progcode,
+     programdesc: row.progdesc,
+     major: row.major,
+     isUndergrad: row.undergrad,
+     isMasteral:row.masteral,
+     isDoctorate: row.phd,
+     isActive:row.is_active
+   });
+
+   if(row.major === null){
+     this.setState({major: ""});
+   }
+   console.log(row);
+   console.log(isSelected);
+   console.log(e);
+ }
+
+openModal(){
+  this.setState({modalIsOpen:true});
+   alert("Giclick");
+}
+
+closeModal(){
+  this.setState({modalIsOpen: false});
+}
+
   render(){
-    let program = this.state.program;
-    const depart = this.state.department.map(obj => obj.deptname );
+
+    const selectRow = {
+          mode:'radio',
+          bgColor: '#80d8ff',
+          onSelect: this.handleRowSelect.bind(this)
+
+    };
+    const customStyles = {
+   content : {
+     top                   : '50%',
+     left                  : '50%',
+     right                 : 'auto',
+     bottom                : 'auto',
+     marginRight           : '-50%',
+     transform             : 'translate(-50%, -50%)'
+   }
+ };
+   const depart = this.state.department.map(obj => obj.deptname );
+
+   console.log(this.state.modalIsOpen);
 
     return(
       <div className="row">
         <div className="Entry">
-            <DropdownList
-              data={depart}
-              name="department"
-              label="Department"
-              value={this.state.deptListData}
-              onChange={value => this.setState({ deptListData: value })} />
+          <img src={addIcon} onClick={this.openModal.bind(this)}/>
+          <img src={saveIcon} alt="Add" />
+          <img src={editIcon} alt="Add" />
+          <img src={cancelIcon} alt="Add" />
+          {/*
 
-            <Input
-              name="programCode"
-              label="Program Code: "
-              type="text"
-              placeholder="Program Code"
-              value={this.state.programCode}
-              onChange={this.handleProgcodechange.bind(this)}/>
+          <Button
+            btnName="Edit" />
 
-            <Select
-                label="Active"
-                checked={this.state.isActive}
-                onChange={this.handleIsactiveChange.bind(this)}/>
+          <Button
+            btnName="Save"
+            onClick={this.saveEdit.bind(this)} />
 
-            <Input
-              name="program"
-              label="Program"
-              type="text"
-              placeholder="Program"
-              value={this.state.programdesc}
-              onChange={this.handleProgramChange.bind(this)} />
+          <Button
+            btnName="Cancel" /> */}
 
-            <Input
-              name="major"
-              label="Major"
-              type="text"
-              placeholder="Major"
-              value={this.state.major}
-              onChange={this.handleMajorChange.bind(this)}/>
+             <Modal
+              isOpen={this.state.modalIsOpen}
+              onRequestClose={this.closeModal}
+              closeTimeoutMS={200}
+              style={customStyles}
+              contentLabel="Add Program"
+              ariaHideApp={false}>
+                <div>
+                  <DropdownList
+                    data={depart}
+                    name="department"
+                    label="Department"
+                    value={this.state.deptListData}
+                    onChange={value => this.setState({ deptListData: value })} />
 
-            <Select
-              label="Undergraduate"
-              checked={this.state.isUndergrad}
-              onChange={this.handleIsundergradChange.bind(this)}/>
+                  <Input
+                    name="programCode"
+                    label="Program Code: "
+                    placeholder="Program Code"
+                    value={this.state.programCode}
+                    onChange={this.handleProgcodechange.bind(this)}/>
 
-            <Select
-              label="Masteral"
-              checked={this.state.isMasteral}
-              onChange={this.handleIsmasteralChange.bind(this)}/>
+                  <Select
+                      label="Active"
+                      checked={this.state.isActive}
+                      onChange={this.handleIsactiveChange.bind(this)}/>
 
-            <Select
-              label="Doctorate"
-              checked={this.state.isDoctorate}
-              onChange={this.handleIsdoctorateChange.bind(this)}/>
+                  <Input
+                    name="program"
+                    label="Program"
 
-            <Button
-              btnName="Add"
-              onClick={this.addProgram.bind(this)}/>
+                    placeholder="Program"
+                    value={this.state.programdesc}
+                    onChange={this.handleProgramChange.bind(this)} />
 
-            <Button
-              btnName="Edit" />
+                  <Input
+                    name="major"
+                    label="Major"
 
-            <Button
-              btnName="Save" />
+                    placeholder="Major"
+                    value={this.state.major}
+                    onChange={this.handleMajorChange.bind(this)}/>
 
-            <Button
-              btnName="Cancel" />
+                  <Select
+                    label="Undergraduate"
+                    checked={this.state.isUndergrad}
+                    onChange={this.handleIsundergradChange.bind(this)}/>
+
+                  <Select
+                    label="Masteral"
+                    checked={this.state.isMasteral}
+                    onChange={this.handleIsmasteralChange.bind(this)}/>
+
+                  <Select
+                    label="Doctorate"
+                    checked={this.state.isDoctorate}
+                    onChange={this.handleIsdoctorateChange.bind(this)}/>
+
+                  <Button
+                      btnName="Add"
+                      onClick={this.addProgram.bind(this)}/>
+                </div>
+
+              </Modal>
+
+
+
         </div>
         <div className="DataView">
-          <Search
+          {/* <Search
             name="search"
             placeholder="Search program. . ."
             value={this.state.searchTerm}
-            onChange={this.handleSearchChange.bind(this)} />
+            onChange={this.handleSearchChange.bind(this)} /> */}
           {/* <TabularData  rowsCount={program.length} data={program} twidth={1050} header1="CODE" header2="PROGRAM" header3="MAJOR" header4="DEPARTMENT"/> */}
-          <Table
+          {/* <Table
               rowsCount={this.state.program.length}
               rowHeight={50}
               width={1050}
@@ -249,7 +360,18 @@ class Program extends React.Component{
                   </Cell>
                 )}
               />
-            </Table>
+            </Table> */}
+
+            <BootstrapTable
+              data={this.state.program}
+              height={600}
+              selectRow={selectRow}>
+                <TableHeaderColumn dataField='progcode' isKey filter={ { type: 'RegexFilter', delay: 1000 } } width="130">CODE</TableHeaderColumn>
+                <TableHeaderColumn dataField='progdesc' width="500">PROGRAM</TableHeaderColumn>
+                <TableHeaderColumn dataField='major' width="170">MAJOR</TableHeaderColumn>
+                <TableHeaderColumn dataField='progdept' width="130">DEPARTMENT</TableHeaderColumn>
+
+            </BootstrapTable>
         </div>
       </div>
     );
